efd72d2a83f459a5fc5b59f33668757cb06ae784
skytwosea <barryjaypenner@gmail.com>
2025-04-06 23:23:42 -0400

    Refactor design to achieve better abstraction
    Scraping is implemented and working for the famous painters layout
    
    The SerpScraper class is being built with two choices for target layout, in order to keep an abstract design in mind. SerpScraper is initialized with the response bytes content and an enum as params; the enum configures the SerpScraper class to target the desired results page layout by setting the correct target scraper function and the correct item tag identifiers.
    
    At first blush, the output file looks good but running diff reveals significant conflicts with files/expected_array.json

--- test_Penner_solution.py
+++ test_Penner_solution.py
@@ -1,10 +1,13 @@
 import pytest
+import bs4
 
 
 from Penner_solution import (
     MockRequests,
-    MockResponse,
-    FamousPainters,
+    # MockResponse,
+    SerpScraper,
+    ScraperTarget,
+    ItemTags
 )
 
 
@@ -19,7 +22,11 @@ def test_mock_headers(mock_get):
     assert headers["Content-Encoding"] == "gzip"
     assert headers["Cache-Control"] == "private"
 
-def test_is_challenge_input_valid(mock_get):
+def test_is_challenge_input_init_is_valid(mock_get):
     content = mock_get.content
-    fp_init = FamousPainters(content)
-    assert fp_init.is_valid
+    scraper = SerpScraper(content, ScraperTarget.PAINTERS)
+    assert scraper._choice == 1
+    assert isinstance(scraper._soup, bs4.BeautifulSoup)
+    assert scraper._target_scraper == scraper._painter_scraper
+    assert isinstance(scraper._tag_ids, ItemTags)
+    assert scraper._validate()
